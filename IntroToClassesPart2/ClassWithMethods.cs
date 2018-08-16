2d1ec7eebd630064a4cad59d02dd158d92b810cf
myasinK <yasinul@gmail.com>
2018-08-15 18:57:45 -0700

    Finish second drill on classes

---
+++ IntroToClassesPart2/ClassWithMethods.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroToClassesPart2
+{
+    public class ClassWithMethods
+    {
+        public void Func(int x, int y)
+        {
+            Console.WriteLine(x+10);
+            Console.WriteLine("The second number was "+ y);
+        }
+    }
+}
---
+++ IntroToClassesPart2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroToClassesPart2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassWithMethods obj = new ClassWithMethods();
+            obj.Func(1,2);
+            obj.Func( x: 1, y: 2 );
+        }
+    }
+}
